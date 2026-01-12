4ca8103b16fc483f55d12300bb870a5e4bedb5e6
Griiimon <25064539+Griiimon@users.noreply.github.com>
2026-01-12 03:35:43 +0100

    Fix unused delta variable warning

--- project/autoloads/scene_manager.gd
+++ project/autoloads/scene_manager.gd
@@ -15,7 +15,7 @@ func _ready() -> void:
 	load_main_menu_scene()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 	
 ## --- public methods ---
