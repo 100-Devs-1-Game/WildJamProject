d4e54046cf81a34cda6bbc8950bb0023b10d9c7b
Veronika Kukk <veronika.kukk@ut.ee>
2026-01-13 20:55:36 +0200

    weapon hotbar code

--- project/modules/hud/hud.gd
+++ project/modules/hud/hud.gd
@@ -9,16 +9,19 @@ extends Node
 # ammo counter
 @export var ammo_count_label: Label
 # weapon hotbar for weapon switching
-# ... todo
+@export var weapon_hotbar: Control
 
+### Those three are just for current debug, can be deleted with along with the timer
 var curr_ammo = 40
 var curr_health = 100.0
 var curr_sanity = 100.0
+###
 
 func _ready() -> void:
 	Signals.change_health_value.connect(set_health_bar_value)
 	Signals.change_sanity_value.connect(set_sanity_bar_value)
 	Signals.change_ammo_count_value.connect(set_ammo_count_value)
+
 
 func set_health_bar_value(current_amount: float) -> void:
 	health_bar.value = current_amount
@@ -29,7 +32,7 @@ func set_sanity_bar_value(current_amount: float) -> void:
 func set_ammo_count_value(current_amount: int, max_amount: int) -> void:
 	ammo_count_label.text = "%s/%s" % [current_amount, max_amount]
 
-# For debug currently
+# For debug currently, can be deleted
 func _on_timer_timeout() -> void:
 	curr_health -= 1.5
 	curr_sanity -= 3.0
