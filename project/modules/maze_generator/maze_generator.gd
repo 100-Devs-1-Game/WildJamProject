85fa964cfdff7d99d48f66b8b86977675e34355f
Griiimon <25064539+Griiimon@users.noreply.github.com>
2026-01-15 15:23:07 +0100

    Store generated map

--- project/modules/maze_generator/maze_generator.gd
+++ project/modules/maze_generator/maze_generator.gd
@@ -1,5 +1,6 @@
 class_name MazeGenerator
 
+static var map: Array
 
 static func generate_maze(width: int, height: int, force_safe_zone: bool = false, safe_zone_pos: Vector2i = Vector2i.ZERO):
 	var tile_gen := TileGenerator.new()
@@ -9,3 +10,14 @@ static func generate_maze(width: int, height: int, force_safe_zone: bool = false
 	for x in range(0,10):
 		tile_gen.cellular_automata()
 	tile_gen.smooth()
+	
+	map = []
+	for y in height:
+		map.append([])
+		for x in width:
+			map[-1].append(true if tile_gen.map[x][y] else false)
+			
+	if force_safe_zone:
+		for x in range(-1, 2):
+			for y in range(-1, 2):
+				map[safe_zone_pos.x + x][safe_zone_pos.y + y] = true
