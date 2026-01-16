b5e8bf07c4484db77335d4f794c3e485b81d99c3
Griiimon <25064539+Griiimon@users.noreply.github.com>
2026-01-16 11:32:09 +0100

    Create EnemyDefinition resource

---
+++ project/modules/enemies/enemy_definition.gd
@@ -0,0 +1,9 @@
+class_name EnemyDefinition
+extends Resource
+
+@export var scene: PackedScene
+@export var spawn_chance: float= 5.0
+
+# TODO
+# item types/amounts to drop, can go into the scene as well if you 
+# dont care about static data separation
